6382d26ca6e48a7742fc6d9da8175647992cba3c
paropeba22 <jhcs0903@gmail.com>
2026-05-04 14:43:12 -0300

    feat: add missing i18n locale files and new Sidebar component, and introduce super admin settings view

--- app/javascript/dashboard/components-next/sidebar/Sidebar.vue
+++ app/javascript/dashboard/components-next/sidebar/Sidebar.vue
@@ -761,7 +761,7 @@ const menuItems = computed(() => {
         </template>
         <template v-else>
           <div class="grid flex-shrink-0 place-content-center size-6">
-            <Logo class="size-4" />
+            <img src="/brand-assets/logo_thumbnail.png" class="size-4" alt="Grupo Telecom" />
           </div>
           <div class="flex-shrink-0 w-px h-3 bg-n-strong" />
           <SidebarAccountSwitcher
